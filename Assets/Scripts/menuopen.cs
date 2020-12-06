3ce0cbcb576240636de3d7e2f99bd7102285aa0f
lastmheart <snep.snepik@gmail.com>
2020-12-06 14:24:37 +0300

    start

---
+++ Assets/Scripts/menuopen.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class menuopen : MonoBehaviour
+{
+    public Animator menu_button;
+    private bool isFirst = false;
+    void Start() {
+       menu_button.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+            if (Input.GetKey(KeyCode.Escape))
+                menu_button.SetBool("isHidden", true);
+    }
+
+    public void click(){
+        if (!isFirst){
+            menu_button.enabled = true;
+            isFirst = true;
+        }
+        else if (menu_button.GetBool("isHidden"))
+            menu_button.SetBool("isHidden", false);
+        else
+            menu_button.SetBool("isHidden", true);
+    }
+}
